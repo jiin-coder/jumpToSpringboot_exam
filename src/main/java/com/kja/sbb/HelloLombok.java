a60a21ae2246a3c50d4593efd5367270ff2c9ef9
kimjiae <fpahstiq123@naver.com>
2022-12-04 20:04:45 +0900

    @RequiredArgsConstructor로 변경

--- src/main/java/com/kja/sbb/HelloLombok.java
+++ src/main/java/com/kja/sbb/HelloLombok.java
@@ -1,19 +1,32 @@
 package com.kja.sbb;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 
+
+/**
+ * @@RequiredArgsConstructor
+ * 1. 필드에 final을 적용 
+ * 2. 위 에노테이션 적용 시
+ * - 해당 필드를 필요로 하는 생성자가 lombok에 의해 자동생성
+ * - final을 적용했기 때문에 @Setter는 의미없음 
+ */
+/**
+ * 아래 코드는 생성자를 직접 작성한 경우 (동일)
+ * public HelloLombok(String hello, int lombok) {
+ * 	this.hello = hello;
+ *  this.lombok = lombok;
+ * }
+ */
+@RequiredArgsConstructor
 @Getter
-@Setter
 public class HelloLombok {
 
-    private String hello;
-    private int lombok;
+    private final String hello;
+    private final int lombok;
 
     public static void main(String[] args) {
-        HelloLombok helloLombok = new HelloLombok();
-        helloLombok.setHello("헬로");
-        helloLombok.setLombok(5);
+        HelloLombok helloLombok = new HelloLombok("헬로", 5);
 
         System.out.println(helloLombok.getHello());
         System.out.println(helloLombok.getLombok());
